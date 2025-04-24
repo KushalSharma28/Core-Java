6c8d2d58061360212ddb047210b2afadd3dd21d7
KushalSharma28 <kushalsharma28499@gmail.com>
2025-04-24 22:46:27 +0530

    New Changes Added

--- src/InterviewPrograms/FibonacciSeries.java
+++ src/InterviewPrograms/FibonacciSeries.java
@@ -6,7 +6,7 @@ public class FibonacciSeries
 {
 	public static void main(String[] args) 
 	{
-		int n, a = 0, b = 0, c = 1;
+		/*int n, a = 0, b = 0, c = 1;
 		Scanner scanner = new Scanner(System.in);
 		        
 		System.out.print("Enter value of n:");
@@ -21,6 +21,23 @@ public class FibonacciSeries
 		     b = c;
 		     c = a + b;
 		     System.out.print(a+" ");
-		}	
+		}	*/
+		
+		int n, a = 0, b = 1, c; // Correct initialization of a and b
+        Scanner scanner = new Scanner(System.in);
+                
+        System.out.print("Enter value of n: ");
+        n = scanner.nextInt();
+        scanner.close();
+                
+        System.out.print("Fibonacci Series: ");
+        
+        for (int i = 1; i <= n; i++) 
+        {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }    
 	}
 }
--- src/InterviewPrograms/FindDuplicates.java
+++ src/InterviewPrograms/FindDuplicates.java
@@ -1,9 +1,12 @@
 package InterviewPrograms;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class FindDuplicates 
 {
 	// Helper function to print a character array
-    private static void printArray(char[] array) 
+    /*private static void printArray(char[] array) 
     {
         for (char element : array) 
         {
@@ -37,6 +40,66 @@ public class FindDuplicates
 
         System.out.println("\nArray 3:");
         printArray(array3);
-	}
+	}*/
 
+	// Input given in the array
+	/*public static void main(String[] args)
+	{
+		int[] array = {1, 2, 3, 4, 5, 2, 3, 6}; 
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+
+        for (int num : array) 
+        {
+            if (!seen.add(num)) 
+            {
+                duplicates.add(num); // If num is already in seen, it's a duplicate
+            }
+        }
+
+        System.out.println("Duplicates in the array: " + duplicates);
+	} */
+	
+	//Taken Input from User in the array
+	public static void main(String[] args) 
+	{
+        Scanner scanner = new Scanner(System.in);
+        
+        // Taking input for the size of the array
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        
+        int[] array = new int[size];
+        
+        // Taking input for the array elements
+        System.out.println("Enter " + size + " elements of the array:");
+        for (int i = 0; i < size; i++)
+        {
+        	array[i] = scanner.nextInt();
+        }
+        
+        // Finding duplicates using HashSet
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+        
+        for (int num : array) 
+        {
+            if (!seen.add(num)) 
+            {
+                duplicates.add(num); // If the number is already in seen, it's a duplicate
+            }
+        }
+        
+        // Displaying the duplicates
+        if (duplicates.isEmpty())
+        {
+            System.out.println("No duplicates found in the array.");
+        } 
+        else 
+        {
+            System.out.println("Duplicates in the array: " + duplicates);
+        }
+
+        scanner.close();
+	}
 }
