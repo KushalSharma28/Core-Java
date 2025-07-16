6ab15d490e8f1e23c76cac78a32c13bcf931d7cd
KushalSharma28 <kushalsharma28499@gmail.com>
2025-07-16 09:23:41 +0530

    New Changes Added

--- src/ArraysPrograms/RemoveElement.java
+++ src/ArraysPrograms/RemoveElement.java
@@ -7,61 +7,6 @@ public class RemoveElement {
 
 	public static void main(String[] args) 
 	{
-		/*
-		int a[] = {31,31,42,63,56,59,67,76,63,31,72,70,80,72};
-		
-		int b[] = new int[a.length-1];
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Which element want to you remove : ");
-		int num = sc.nextInt();
-	
-		int min = Integer.MIN_VALUE;
-		boolean flag = true;
-		while(flag)
-		{	
-			int s=-1;
-		
-			for(int i=0;i<a.length;i++)
-			{
-				if(num==a[i]) 
-				{
-					s = i;
-					a[i]=min;
-				}
-			}
-		
-			if(s==-1)
-			{
-				flag = false;
-				break;
-			}
-			else
-			{
-				for(int i=0;i<b.length;i++)
-				{
-					if(i<s)
-					{		 
-						b[i] = a[i];
-					}
-					else
-					{
-						b[i] = a[i+1];
-					}
-				}
-		
-			}	
-		
-		}
-
-		for(int i=0;i<b.length;i++)
-		{
-			if(a[i]!=min)
-			{
-				System.out.println(b[i]);		
-			}
-		}
-		 */
 		int[] originalArray = {677, 12, 233, 78, 45};
         int elementToRemove = 78;
         int[] newArray = new int[originalArray.length - 1]; // New array will be one element smaller
