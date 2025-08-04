5b713f6ff85eaa4d0b14610232bb3fe967b1a8db
KushalSharma28 <kushalsharma28499@gmail.com>
2025-08-04 16:41:56 +0530

    New Change Added

--- src/InterviewPrograms/WordFrequency.java
+++ src/InterviewPrograms/WordFrequency.java
@@ -1,9 +1,12 @@
 package InterviewPrograms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordFrequency 
 {
-
-	public static void main(String[] args) 
+	// Word Frequency using Manual Iteration
+	/*public static void main(String[] args) 
 	{
 		String str = "this is test this is another test";
         String[] words = str.split(" ");
@@ -45,6 +48,29 @@ public class WordFrequency
             System.out.println(uniqueWords[i] + " : " + counts[i]);
         }
 
-	}
+	}*/
 
+	//Word frequency using Hash Map
+	public static void main(String[] args) 
+	{
+		String input = "this is test this is another test";
+
+		// Convert to lowercase and split by whitespace
+		String[] words = input.toLowerCase().split("\\s+");
+
+		// Create a HashMap to store word frequencies
+		Map<String, Integer> wordCount = new HashMap<>();
+
+		// Count occurrences
+		for (String word : words) 
+		{
+			wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+		}
+
+		// Print word frequencies
+		for (Map.Entry<String, Integer> entry : wordCount.entrySet()) 
+		{
+			System.out.println(entry.getKey() + " -> " + entry.getValue());
+		}
+	}
 }
