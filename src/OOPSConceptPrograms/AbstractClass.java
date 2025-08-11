5a62ea6f795bdabc4fee6a5abbc252f4b4731ceb
KushalSharma28 <kushalsharma28499@gmail.com>
2025-08-11 17:06:08 +0530

    New File Added

--- src/OOPSConceptPrograms/AbstractClass.java
+++ src/OOPSConceptPrograms/AbstractClass.java
@@ -4,31 +4,30 @@ package OOPSConceptPrograms;
 //Abstract class
 abstract class Politician 
 {
- // Abstract method (no body)
- public abstract void DontCare();
+	// Abstract method (no body)
+	public abstract void DontCare();
 
- // Regular method
- public void Care() 
- {
-     System.out.println("Care for People.");
- }
+	// Regular method
+	public void Care() 
+	{
+		System.out.println("Care for People.");
+	}
 }
 
 //Concrete subclass
 class MLA extends Politician {
- // Implement abstract method
- public void DontCare() {
+	// Implement abstract method
+	public void DontCare() {
      System.out.println("Don't Care for People After Election");
- }
+	}
 }
 
 public class AbstractClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Politician a = new MLA();
-        a.Care();  // Calls Dog's sound()
-        a.DontCare();  // Calls Animal's sleep()
-
+        a.Care();  
+        a.DontCare();  
 	}
-
 }
