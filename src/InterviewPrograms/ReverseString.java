a6ec5c77df599db222e2bcb21461fcb9b8b50aa0
KushalSharma28 <kushalsharma28499@gmail.com>
2025-08-13 11:44:10 +0530

    Reverse String Program Update

--- src/InterviewPrograms/ReverseString.java
+++ src/InterviewPrograms/ReverseString.java
@@ -6,7 +6,7 @@ public class ReverseString
 {
 	public static void main(String[] args) 
 	{
-		String str;
+		/*String str;
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Enter the String");
@@ -28,7 +28,35 @@ public class ReverseString
 			output=output+reverse+" ";
 		}
 		System.out.println("Reverse string of " + str +" is ="+ output);
-		sc.close();
+		sc.close();*/
+		
+		Scanner sc = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+        
+        /*String reversed = "";
+
+        // Loop from last character to first
+        for (int i = original.length() - 1; i >= 0; i--) 
+        {
+            reversed += original.charAt(i); // append each char from end
+        }
+
+        // Display the reversed string
+        System.out.println("Reversed string: " + reversed);
+
+        sc.close(); */
+        
+        char[] chars = original.toCharArray();
+        String reversed = "";
+        for (int i = chars.length - 1; i >= 0; i--) 
+        {
+            reversed += chars[i];
+        }
+        System.out.println("Reversed string: " + reversed);
+        sc.close();
 	}
 
 }
